cf0554cce3e008145d0f779988f48ef5a4072cae
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-09-08 15:40:58 -0300

    08/09/2023-15:40

--- Projeto-Contador/src/ProjetoContador/AnalisandoCadidatos/analisandoCandidatos.java
+++ Projeto-Contador/src/ProjetoContador/AnalisandoCadidatos/analisandoCandidatos.java
@@ -1,4 +1,4 @@
-package ProjetoContador;
+package ProjetoContador.AnalisandoCadidatos;
 
 
 
---
+++ Projeto-Contador/src/ProjetoContador/SelecionandoCandidatos/selecaodeCandidatos.java
@@ -0,0 +1,34 @@
+package ProjetoContador.SelecionandoCandidatos;
+
+import java.util.concurrent.ThreadLocalRandom;
+public class selecaodeCandidatos {
+    public static void main(String[] args) {
+        selecaoodeCandidatos();
+
+    }
+
+    static void selecaoodeCandidatos() {
+        String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO", "MIRELA",
+                "DANIELA", "JORGE" };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicito este valor de salário " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+}
